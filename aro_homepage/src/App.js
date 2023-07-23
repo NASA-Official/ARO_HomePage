e5923421a8f9753db6ec6e546956bb977b24ce4b
chang0-0 <bigyoung8375@naver.com>
2023-07-23 14:18:28 +0900

    force push

---
+++ aro_homepage/src/App.js
@@ -0,0 +1,15 @@
+import "./App.scss";
+import ProgressBar from "./components/ProgressBar";
+
+function App() {
+  return (
+    <div className="App">
+      <ProgressBar />
+      <body>
+      </body>
+        <div>ARO</div>
+    </div>
+  );
+}
+
+export default App;
